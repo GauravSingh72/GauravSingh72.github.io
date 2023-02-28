bb6b7d2f49d8520fc29b6c64f62d718447e3a3d7
GauravSingh72 <1gaurav2singh@gmail.com>
2023-02-28 11:31:17 +0530

    Portfolio

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,9 +1,3 @@
-// const downloadResume = () => {
-//   window.open(
-//     "https://drive.google.com/file/d/1IaDqlnbpasqpYKWgKCiefdHVKkEduFNd/view?usp=share_link",
-//     "_blank"
-//   );
-// };
 
 const btn = document.getElementById("resumeDownloadBtn");
 btn.addEventListener("click", function (ele) {
