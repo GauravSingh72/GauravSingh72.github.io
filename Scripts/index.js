4fef058b6aab319fce8624ebe783b0388c4f2897
GauravSingh72 <1gaurav2singh@gmail.com>
2023-03-17 18:55:11 +0530

    Portfolio

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,8 +1,14 @@
-const btn1 = document.getElementById("resume-link-2")
+const btn1 = document.getElementById("resume-link-1")
 function myFunction() {
   window.open(
     "https://drive.google.com/file/d/1IaDqlnbpasqpYKWgKCiefdHVKkEduFNd/view?usp=share_link"
   )
 }
 
+const btn2 = document.getElementById("resume-link-2")
+function myFunction2() {
+  window.open(
+    "https://drive.google.com/file/d/1IaDqlnbpasqpYKWgKCiefdHVKkEduFNd/view?usp=share_link"
+  )
+}
 
