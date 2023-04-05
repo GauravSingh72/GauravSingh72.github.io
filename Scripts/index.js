e90f089f348eba503c2e21846629a20fb44a021c
GauravSingh72 <1gaurav2singh@gmail.com>
2023-04-05 15:14:10 +0530

    Portfolio

--- Scripts/index.js
+++ Scripts/index.js
@@ -5,12 +5,12 @@ function myFunction() {
   )
 }
 
-// const btn2 = document.getElementById("resume-link-2")
-// function myFunction2() {
-//   window.open(
-//     "https://drive.google.com/file/d/1IaDqlnbpasqpYKWgKCiefdHVKkEduFNd/view?usp=share_link"
-//   )
-// }
+const btn2 = document.getElementById("resume-link-2")
+function myFunction2() {
+  window.open(
+    "https://drive.google.com/file/d/1IaDqlnbpasqpYKWgKCiefdHVKkEduFNd/view?usp=share_link"
+  )
+}
 
 // const btn3 = document.getElementById("resume-link-2")
 // function myFunction3() {
@@ -26,8 +26,8 @@ function myFunction() {
 //   window.location.href="https://drive.google.com/u/0/uc?id=1WSBIQkMN-qUIUQN3S_E7WP0IYmIyiLuH&export=download";
 // })
 
-document.querySelector("#resume-button-2").addEventListener("click",()=>{
-  // console.log("2")
-  window.location.href="https://drive.google.com/u/0/uc?id=1IaDqlnbpasqpYKWgKCiefdHVKkEduFNd&export=download";
-  window.open("https://drive.google.com/file/d/1IaDqlnbpasqpYKWgKCiefdHVKkEduFNd/view","_blank") ;
-})
+// document.querySelector("#resume-button-2").addEventListener("click",()=>{
+//   // console.log("2")
+//   window.location.href="https://drive.google.com/u/0/uc?id=1IaDqlnbpasqpYKWgKCiefdHVKkEduFNd&export=download";
+//   window.open("https://drive.google.com/file/d/1IaDqlnbpasqpYKWgKCiefdHVKkEduFNd/view","_blank") ;
+// })
