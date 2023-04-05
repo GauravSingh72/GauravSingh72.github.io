3478c09c762a215b65c0c33449642962fe25173f
GauravSingh72 <1gaurav2singh@gmail.com>
2023-04-05 19:24:38 +0530

    Portfolio

--- Scripts/index.js
+++ Scripts/index.js
@@ -11,23 +11,3 @@ function myFunction2() {
     "https://drive.google.com/file/d/1IaDqlnbpasqpYKWgKCiefdHVKkEduFNd/view?usp=share_link"
   )
 }
-
-// const btn3 = document.getElementById("resume-link-2")
-// function myFunction3() {
-//   window.open(
-//     "https://drive.google.com/file/d/1IaDqlnbpasqpYKWgKCiefdHVKkEduFNd/view?usp=share_link"
-//   )
-// }
-
-// document.querySelector("#resume-button-1").addEventListener("click",()=>{
-//   // console.log("1")
- 
-//   window.open("https://drive.google.com/file/d/1WSBIQkMN-qUIUQN3S_E7WP0IYmIyiLuH/view","_blank") ;
-//   window.location.href="https://drive.google.com/u/0/uc?id=1WSBIQkMN-qUIUQN3S_E7WP0IYmIyiLuH&export=download";
-// })
-
-// document.querySelector("#resume-button-2").addEventListener("click",()=>{
-//   // console.log("2")
-//   window.location.href="https://drive.google.com/u/0/uc?id=1IaDqlnbpasqpYKWgKCiefdHVKkEduFNd&export=download";
-//   window.open("https://drive.google.com/file/d/1IaDqlnbpasqpYKWgKCiefdHVKkEduFNd/view","_blank") ;
-// })
