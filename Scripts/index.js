cd9d7b0a90b3e4b38e150a89eb7c4a621454301d
GauravSingh72 <1gaurav2singh@gmail.com>
2023-09-13 19:38:08 +0530

    Updated Projects & Resume

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,13 +1,13 @@
 const btn1 = document.getElementById("resume-link-1")
 function myFunction() {
   window.open(
-    "https://drive.google.com/file/d/16CAVVxgfaoRQzTMy1QgqQi6_k2ekwjd1/view?usp=share_link"
+    "https://drive.google.com/file/d/16CAVVxgfaoRQzTMy1QgqQi6_k2ekwjd1/view?usp=sharing"
   )
 }
 
 const btn2 = document.getElementById("resume-link-2")
 function myFunction2() {
   window.open(
-    "https://drive.google.com/file/d/16CAVVxgfaoRQzTMy1QgqQi6_k2ekwjd1/view?usp=share_link"
+    "https://drive.google.com/file/d/16CAVVxgfaoRQzTMy1QgqQi6_k2ekwjd1/view?usp=sharing"
   )
 }
